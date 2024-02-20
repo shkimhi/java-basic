2160a4c1b9b3a9797a564608bab5e374d7ccaedf
shkim <sh.kim@tilon.com>
2024-02-21 08:57:12 +0900

    클래스 도입 - 객체배열

--- src/class1/ClassStart3.java
+++ src/class1/ClassStart3.java
@@ -16,5 +16,7 @@ public class ClassStart3 {
 
         System.out.println("이름:" + student1.name + " 나이:" + student1.age + " 성적:" + student1.grade);
         System.out.println("이름:" + student2.name + " 나이:" + student2.age + " 성적:" + student2.grade);
+        System.out.println(student1);
+        System.out.println(student2);
     }
 }
