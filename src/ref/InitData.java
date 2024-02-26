7a6d4c905b5c896ec494f1a5bc5ba55c5b0c25e1
shkim <ajfqhksh1@gmail.com>
2024-02-26 13:34:49 +0900

    변수초기화

---
+++ src/ref/InitData.java
@@ -0,0 +1,7 @@
+package ref;
+
+public class InitData {
+    int val1; //초기화 X
+    int val2 = 10; //초기화 O
+
+}
---
+++ src/ref/InitMain.java
@@ -0,0 +1,11 @@
+package ref;
+
+public class InitMain {
+
+    public static void main(String[] args) {
+        InitData initData =new InitData();
+        System.out.println(initData.val1);
+        System.out.println(initData.val2);
+    }
+
+}
