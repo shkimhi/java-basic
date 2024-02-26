226a60a2b9837a3d739239afdadcf4e752e457dc
shkim <ajfqhksh1@gmail.com>
2024-02-26 09:17:42 +0900

    기본형, 참조형 추가

---
+++ src/ref/MethodChange1.java
@@ -0,0 +1,18 @@
+package ref;
+
+public class MethodChange1 {
+
+    public static void main(String[] args) {
+
+        int a= 10;
+        System.out.println(a);
+        a = changePrimitive(a);
+        System.out.println(a);
+
+    }
+
+    static int changePrimitive(int x){
+        x = 20;
+        return x;
+    }
+}
