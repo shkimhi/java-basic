74e8326a441a7275627894f7b53066f537c83464
shkim <ajfqhksh1@gmail.com>
2024-03-05 08:34:41 +0900

    상속관계1

---
+++ src/extends1/ex1/GasCar.java
@@ -0,0 +1,11 @@
+package extends1.ex1;
+
+public class GasCar {
+
+    public void move(){
+        System.out.println("이동");
+    }
+    public void fillUp(){
+        System.out.println("주유");
+    }
+}
