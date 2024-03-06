cbc85d30a5eb0a4e49292397a70095d1c8cef1e3
shkim <ajfqhksh1@gmail.com>
2024-03-06 10:42:18 +0900

    실체가 없는 객체 생성, Overriding 누락 실수 를 추상클래스, 추상메서드 로 개선

---
+++ src/poly/ex3/AbstractMain.java
@@ -0,0 +1,22 @@
+package poly.ex3;
+
+public class AbstractMain {
+
+    public static void main(String[] args) {
+        //추상 클래스 생성 불가
+        //AbstractAnimal animal = new AbstractAnimal();
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(caw);
+
+    }
+
+    private static void soundAnimal(AbstractAnimal animal){
+        animal.sound();
+    }
+}
